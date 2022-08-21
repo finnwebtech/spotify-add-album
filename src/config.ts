7c8621c8cd8fe9f93dde162f4865f7ae57c231f4
finnweb <github@finnweb.tech>
2022-08-21 17:42:10 +0200

    add config for constants

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export default {
+    CLIENT_ID: process.env.CLIENT_ID as string,
+    CLIENT_SECRET: process.env.CLIENT_SECRET as string,
+    SPOTIFY_AUTH_URL: process.env.SPOTIFY_AUTH_URL as string,
+    SPOTIFY_TOKEN_URL: process.env.SPOTIFY_TOKEN_URL as string,
+    SPOTIFY_SAFE_ALBUM_URL: process.env.SPOTIFY_SAFE_ALBUM_URL as string,
+    SPOTIFY_SEARCH_URL: process.env.SPOTIFY_SEARCH_URL as string,
+    SPOTIFY_USERNAME: process.env.SPOTIFY_USERNAME as string,
+    SPOTIFY_MAIL: process.env.SPOTIFY_MAIL as string,
+    SPOTIFY_PW: process.env.SPOTIFY_PW as string,
+    SPOTIFY_REDIRECT_URL: process.env.SPOTIFY_REDIRECT_URL as string
+};
