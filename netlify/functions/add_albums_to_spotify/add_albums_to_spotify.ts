e7d722b477dfa07e7c592bd52df441a94d743aeb
finnweb <github@finnweb.tech>
2022-08-22 21:08:10 +0200

    run every minute

--- netlify/functions/add_albums_to_spotify/add_albums_to_spotify.ts
+++ netlify/functions/add_albums_to_spotify/add_albums_to_spotify.ts
@@ -6,7 +6,7 @@ import { addAlbumsToSpotify, login } from "../../../src/spotify-helper.js"
 
 // To learn about scheduled functions and supported cron extensions, 
 // see: https://ntl.fyi/sched-func
-export const handler = schedule("@hourly", async (event) => {
+export const handler = schedule("* * * * *", async (event) => {
     const eventBody = JSON.parse(event.body || "");
     console.log(`Next function run at ${eventBody.next_run}.`);
 
